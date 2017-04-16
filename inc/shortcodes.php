a0dc9ce2f7b7db97319a58ce63497cafd5d32a13
Aleksey Druzhinin <alexey@yourtask.ru>
2017-04-16 20:40:07 +0300

    Fix shortcode output. See https://codex.wordpress.org/Shortcode_API.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -10,6 +10,7 @@
  * Usage: [goods_sitemap]
  */
 function GoodsSitemap($atts) {
+	$output='';
 	// Attributes
 	extract(shortcode_atts(
 		array(
@@ -28,7 +29,7 @@ function GoodsSitemap($atts) {
 	);
 	$terms = get_terms('goods_category', $terms_args);
 	if ($terms) :
-		echo '<ul class="parents">';
+		$output .= '<ul class="parents">';
 		foreach ($terms as $term) :
 			if ($parent = $term->parent) :
 				$class = 'child';
@@ -36,9 +37,9 @@ function GoodsSitemap($atts) {
 				$class = 'parent';
 			endif;
 
-			echo '<li class="' . $class . '"><h3><a href="' . get_term_link($term) . '">' . $term->name . '</a></h3></li>';
+			$output .= '<li class="' . $class . '"><h3><a href="' . get_term_link($term) . '">' . $term->name . '</a></h3></li>';
 			$parent = $term->parent;
-			echo '<ul>';
+			$output .= '<ul>';
 			$query_args = array(
 				'post_type' => 'goods',
 				'posts_per_page' => -1,
@@ -54,12 +55,13 @@ function GoodsSitemap($atts) {
 			$new = new WP_Query($query_args);
 			while ($new->have_posts()) :
 				$new->the_post();
-				echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+				$output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
 			endwhile;
-			echo '</ul>';
+			$output .= '</ul>';
 		endforeach;
-		echo '</ul>';
+		$output .= '</ul>';
 	endif;
+	return $output;
 }
 
 add_shortcode('goods_sitemap', 'GoodsSitemap');
